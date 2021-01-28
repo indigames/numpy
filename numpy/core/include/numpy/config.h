f52a023dc0c4bc2d6f591fa0db7a9e01bf41e4ac
thai.phi <thai.phi@indigames.net>
2021-01-28 16:08:39 +0700

    Fix linking error on Android.

--- numpy/core/include/numpy/config.h
+++ numpy/core/include/numpy/config.h
@@ -42,17 +42,17 @@
 #define HAVE_CBRT 1
 
 /// [IGE]: Only Windows
-#ifdef WIN32
+#if defined(WIN32)
 #define HAVE_XMMINTRIN_H 1
 #define HAVE_EMMINTRIN_H 1
 #define HAVE_IMMINTRIN_H 1
 #define HAVE__MM_LOAD_PS 1
 #define HAVE__MM_PREFETCH 1
 #define HAVE__MM_LOAD_PD 1
-#define HAVE___DECLSPEC_THREAD_ 1
 #endif
 /// [/IGE]
 
+#define HAVE___DECLSPEC_THREAD_ 1
 #define HAVE_SINF 1
 #define HAVE_COSF 1
 #define HAVE_TANF 1
@@ -99,72 +99,91 @@
 #define HAVE_DECL_SIGNBIT
 // #define HAVE_DECL_ISFINITE ///[IGE]: redefinition
 #define HAVE_COMPLEX_H 1
-#define HAVE_CABS 1
-#define HAVE_CACOS 1
-#define HAVE_CACOSH 1
-#define HAVE_CARG 1
-#define HAVE_CASIN 1
-#define HAVE_CASINH 1
-#define HAVE_CATAN 1
-#define HAVE_CATANH 1
-#define HAVE_CCOS 1
-#define HAVE_CCOSH 1
-#define HAVE_CEXP 1
-#define HAVE_CIMAG 1
-#define HAVE_CLOG 1
-#define HAVE_CONJ 1
-#define HAVE_CPOW 1
-#define HAVE_CPROJ 1
-#define HAVE_CREAL 1
-#define HAVE_CSIN 1
-#define HAVE_CSINH 1
-#define HAVE_CSQRT 1
-#define HAVE_CTAN 1
-#define HAVE_CTANH 1
-#define HAVE_CABSF 1
-#define HAVE_CACOSF 1
-#define HAVE_CACOSHF 1
-#define HAVE_CARGF 1
-#define HAVE_CASINF 1
-#define HAVE_CASINHF 1
-#define HAVE_CATANF 1
-#define HAVE_CATANHF 1
-#define HAVE_CCOSF 1
-#define HAVE_CCOSHF 1
-#define HAVE_CEXPF 1
-#define HAVE_CIMAGF 1
-#define HAVE_CLOGF 1
-#define HAVE_CONJF 1
-#define HAVE_CPOWF 1
-#define HAVE_CPROJF 1
-#define HAVE_CREALF 1
-#define HAVE_CSINF 1
-#define HAVE_CSINHF 1
-#define HAVE_CSQRTF 1
-#define HAVE_CTANF 1
-#define HAVE_CTANHF 1
-#define HAVE_CABSL 1
-#define HAVE_CACOSL 1
-#define HAVE_CACOSHL 1
-#define HAVE_CARGL 1
-#define HAVE_CASINL 1
-#define HAVE_CASINHL 1
-#define HAVE_CATANL 1
-#define HAVE_CATANHL 1
-#define HAVE_CCOSL 1
-#define HAVE_CCOSHL 1
-#define HAVE_CEXPL 1
-#define HAVE_CIMAGL 1
-#define HAVE_CLOGL 1
-#define HAVE_CONJL 1
-#define HAVE_CPOWL 1
-#define HAVE_CPROJL 1
-#define HAVE_CREALL 1
-#define HAVE_CSINL 1
-#define HAVE_CSINHL 1
-#define HAVE_CSQRTL 1
-#define HAVE_CTANL 1
-#define HAVE_CTANHL 1
+
+/// [IGE]: Fix android linking
+#if defined(__ANDROID__)
+    #define HAVE_CIMAG 1
+    #define HAVE_CONJ 1
+    #define HAVE_CPROJ 1
+    #define HAVE_CREAL 1
+    #define HAVE_CIMAGF 1
+    #define HAVE_CONJF 1
+    #define HAVE_CPROJF 1
+    #define HAVE_CREALF 1
+    #define HAVE_CIMAGL 1
+    #define HAVE_CONJL 1
+    #define HAVE_CPROJL 1
+    #define HAVE_CREALL 1
+#else
+    #define HAVE_CABS 1
+    #define HAVE_CACOS 1
+    #define HAVE_CACOSH 1
+    #define HAVE_CARG 1
+    #define HAVE_CASIN 1
+    #define HAVE_CASINH 1
+    #define HAVE_CATAN 1
+    #define HAVE_CATANH 1
+    #define HAVE_CCOS 1
+    #define HAVE_CCOSH 1
+    #define HAVE_CEXP 1
+    #define HAVE_CIMAG 1
+    #define HAVE_CLOG 1
+    #define HAVE_CONJ 1
+    #define HAVE_CPOW 1
+    #define HAVE_CPROJ 1
+    #define HAVE_CREAL 1
+    #define HAVE_CSIN 1
+    #define HAVE_CSINH 1
+    #define HAVE_CSQRT 1
+    #define HAVE_CTAN 1
+    #define HAVE_CTANH 1
+    #define HAVE_CABSF 1
+    #define HAVE_CACOSF 1
+    #define HAVE_CACOSHF 1
+    #define HAVE_CARGF 1
+    #define HAVE_CASINF 1
+    #define HAVE_CASINHF 1
+    #define HAVE_CATANF 1
+    #define HAVE_CATANHF 1
+    #define HAVE_CCOSF 1
+    #define HAVE_CCOSHF 1
+    #define HAVE_CEXPF 1
+    #define HAVE_CIMAGF 1
+    #define HAVE_CLOGF 1
+    #define HAVE_CONJF 1
+    #define HAVE_CPOWF 1
+    #define HAVE_CPROJF 1
+    #define HAVE_CREALF 1
+    #define HAVE_CSINF 1
+    #define HAVE_CSINHF 1
+    #define HAVE_CSQRTF 1
+    #define HAVE_CTANF 1
+    #define HAVE_CTANHF 1
+    #define HAVE_CABSL 1
+    #define HAVE_CACOSL 1
+    #define HAVE_CACOSHL 1
+    #define HAVE_CARGL 1
+    #define HAVE_CASINL 1
+    #define HAVE_CASINHL 1
+    #define HAVE_CATANL 1
+    #define HAVE_CATANHL 1
+    #define HAVE_CCOSL 1
+    #define HAVE_CCOSHL 1
+    #define HAVE_CEXPL 1
+    #define HAVE_CIMAGL 1
+    #define HAVE_CLOGL 1
+    #define HAVE_CONJL 1
+    #define HAVE_CPOWL 1
+    #define HAVE_CPROJL 1
+    #define HAVE_CREALL 1
+    #define HAVE_CSINL 1
+    #define HAVE_CSINHL 1
+    #define HAVE_CSQRTL 1
+    #define HAVE_CTANL 1
+    #define HAVE_CTANHL 1
+#endif // __ANDROID__
+/// [/IGE]
+
 #define __NPY_PRIVATE_NO_SIGNAL
 #define DISTUTILS_USE_SDK
 #define FORCE_NO_LONG_DOUBLE_FORMATTING
